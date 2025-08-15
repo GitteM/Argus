0195ffce6cbc2c02c9ffb00ac069cf38ff6f44b7
Brigitte Michau <brigitte.michau@icloud.com>
2025-08-15 16:10:35 +0300

    Initial Commit

---
+++ Argus/ArgusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgusApp.swift
+//  Argus
+//
+//  Created by Brigitte Michau on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArgusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArgusTests/ArgusTests.swift
@@ -0,0 +1,16 @@
+//
+//  ArgusTests.swift
+//  ArgusTests
+//
+//  Created by Brigitte Michau on 15.08.2025.
+//
+
+import Testing
+
+struct ArgusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
